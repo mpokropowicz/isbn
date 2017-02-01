7d449ae7dfb3fcb5a3277ec2e5a762de7bd6ced6
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-01 17:20:27 -0500

    correct_checksum and is_good_checksum pass

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -12,3 +12,33 @@ def remove_spaces_and_dashes(num)
 
 	formatted_number
 end
+
+def calculate_checksum(num)
+
+	sum = 0
+	index = 1
+
+	if num[num.length - 1].upcase == "X"
+
+		num.delete!(num[num.length - 1])
+	end
+
+	num.each_char do |number|
+
+		sum += (number.to_i * index)
+		index += 1
+	end
+
+	sum % 11
+end
+
+def correct_checksum?(num)
+
+	if num[num.length - 1].upcase == "X"
+
+		calculate_checksum(num) == 10
+	else
+
+		calculate_checksum(num) == num[length - 1]
+	end
+end
--- test_isbn_validator.rb
+++ test_isbn_validator.rb
@@ -7,4 +7,14 @@ class TestISBN < Minitest::Test
 
 		assert_equal("0471958697", remove_spaces_and_dashes("04 7-1-9 58 69-7"))
 	end
+
+	def test_correct_checksum
+
+		assert_equal(7, calculate_checksum("0471958690"))
+	end
+
+	def test_is_good_checksum
+
+		assert_equal(true, correct_checksum?("877195869x"))
+	end
 end
