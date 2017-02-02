ff186ec47167a804c02d04005fe612c1a525f9d8
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-02 18:22:46 -0500

    all tests validate for ISBN10

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -40,11 +40,28 @@ def correct_checksum?(num)
 	end
 end
 
+def contains_nonvalids?(num)
+
+	valid_chars = "xX0123456789"
+	num = num.split("")
+	num_valid_chars = 0
+
+	num.each do |e|
+
+		if valid_chars.include? e
+
+			num_valid_chars += 1
+		end
+	end
+
+	valid_chars ==  num.length
+end
+
 def is_valid_ISBN_10?(num)
 
 	num = remove_spaces_and_dashes(num)
 
-	if num.length == 10 && correct_checksum?(num)
+	if num.length == 10 && correct_checksum?(num) && !contains_nonvalids?(num)
 
 		return true
 	else
@@ -54,6 +71,6 @@ def is_valid_ISBN_10?(num)
 end
 # # puts remove_spaces_and_dashes("0471958697")
 
-# print is_valid_ISBN_10?("0471958697")
+#print is_valid_ISBN_10?("0471958697")
 
 # #print calculate_checksum("0471958697")
