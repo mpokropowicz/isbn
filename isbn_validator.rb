44c126148c8788b542cc3653fc0c55f648b6dd27
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-04 13:57:03 -0500

    fixed correct x for non-x isbn

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -73,7 +73,12 @@ end
 
 def correct_x?(num)
 
-	num.upcase.index("X") == num.length - 1
+	if num.upcase.include? "X"
+
+		num.upcase.index("X") == num.length - 1
+	end
+	
+	true
 end
 
 def is_valid_ISBN_10?(num)
@@ -118,7 +123,7 @@ def is_valid_ISBN?(num)
 end
 
 #puts remove_spaces_and_dashes("0471958697")
-#print correct_x?("877195869x")
-#print correct_checksum?("0471958697")
+print correct_x?("0471958697")
+print correct_checksum?("0471958697")
 # puts calculate_checksum("9782207258040")
 # print calculate_checksum("9780470059029")
