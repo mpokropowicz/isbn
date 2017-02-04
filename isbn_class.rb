3f7cf5f256bcffb85789934226f8a6d29789b2dc
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-04 13:50:27 -0500

    handeled some errors and refacatored a bit

--- isbn_class.rb
+++ isbn_class.rb
@@ -42,11 +42,9 @@ def get_checksum()
 	(sum % 11).to_s
 end
 
-def length
+def get_length
 
 	@number.length
 end
-
-
 
 end
--- isbn_validator.rb
+++ isbn_validator.rb
@@ -71,21 +71,52 @@ def contains_nonvalids?(num)
 	valid_chars ==  num.length
 end
 
-def is_valid_ISBN(num)
+def correct_x?(num)
+
+	num.upcase.index("X") == num.length - 1
+end
+
+def is_valid_ISBN_10?(num)
+
+	num = remove_spaces_and_dashes(num)
+
+	if correct_checksum?(num) && !contains_nonvalids?(num) && correct_x?(num)
+
+		return true
+	end
+
+	false
+end
+
+
+def is_valid_ISBN_13?(num)
 
 	num = remove_spaces_and_dashes(num)
 
 	if correct_checksum?(num) && !contains_nonvalids?(num)
 
 		return true
-	else
+	end
 
-		return false
+	false
+end
+
+
+def is_valid_ISBN?(num)
+
+	if num.length == 10
+
+		is_valid_ISBN_10?(num)
+	end
+
+	if num.length == 13
+
+		is_valid_ISBN_13?(num)
 	end
 end
-# # puts remove_spaces_and_dashes("0471958697")
 
-#print is_valid_ISBN_10?("0471958697")
+#puts remove_spaces_and_dashes("0471958697")
+#print correct_x?("877195869x")
 #print correct_checksum?("0471958697")
-puts calculate_checksum("9782207258040")
-print calculate_checksum("9780470059029")
+# puts calculate_checksum("9782207258040")
+# print calculate_checksum("9780470059029")
